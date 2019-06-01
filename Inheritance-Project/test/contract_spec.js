c84cbf2a1e73080661d4e5a6676a0e5fc4f511eb
Natalie Chin <chinnh@mcmaster.ca>
2019-05-31 20:33:57 -0400

    Added additional tests to ensure deployment worked

--- Inheritance-Project/test/contract_spec.js
+++ Inheritance-Project/test/contract_spec.js
@@ -4,16 +4,12 @@ const TipJar = require('Embark/contracts/TipJar');
 
 let accounts;
 
-// For documentation please see https://embark.status.im/docs/contracts_testing.html
 config({
-  //deployment: {
-  //  accounts: [
-  //    // you can configure custom accounts with a custom balance
-  //    // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
-  //  ]
-  //},
   contracts: {
-    TipJar: {}
+    ownerAbstract: {}, 
+    eventInterface: {},
+    TipJar: {},
+    Owner: {fromIndex:0}
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
@@ -21,7 +17,19 @@ config({
 
 contract("TipJar", function () {
   this.timeout(0);
+
+  it("should deploy the owner contract", async function() {
+    let result = Owner.options.address;
+    console.log("---------------------", result);
+//    assert.ok(result);
+  });
   
+  it("should deploy the tipjar", async function() {
+    let result = TipJar.options.address;
+    console.log(result);
+    assert.ok(result);
+  });
+
   it("get owner address", async function () {
     let result = await TipJar.methods.showOwner().call();
     assert.ok(result);
